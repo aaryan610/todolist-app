14077d1511c581022a030e88511a0eabe157e3f9
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2022-10-04 17:10:16 +0530

    Strucutre created

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,40 @@
-import { useState } from "react";
+import { list } from "postcss";
+import { useState, useEffect } from "react";
 import SingleItem from "../components/single-item";
 
-const Home = () => {
-  const [item, setItem] = useState();
+const Home = (props) => {
+  const [name, setName] = useState();
+  const [date, setDate] = useState();
+  const [list, setList] = useState([]);
 
-  const handleSubmit = (e) => {
+  // Add item to the list
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Add item code here
+    // Clear input fields after adding
+    setName("");
+    setDate("");
   };
 
   return (
     <>
       <main className="grid place-items-center h-screen">
         <div className="container bg-white rounded-lg p-4 w-1/2 my-4 h-1/2 overflow-y-scroll">
-          <form onSubmit={handleSubmit} className="grid grid-cols-3 gap-2">
+          <form onSubmit={handleSubmit} className="grid grid-cols-2 gap-2">
             <input
               type="text"
-              className="border rounded h-full py-2 px-3 text-gray-700 mb-3 focus:outline-none col-span-2"
-              onChange={(e) => setItem(e.target.value)}
+              className="border rounded h-full py-2 px-3 text-gray-700 mb-3 focus:outline-none"
+              placeholder="Enter Title"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+            <input
+              type="date"
+              className="border rounded h-full py-2 px-3 text-gray-700 mb-3 focus:outline-none"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              required
             />
             <button
               type="submit"
@@ -28,8 +44,9 @@ const Home = () => {
             </button>
           </form>
           <div className="list mt-4">
-            <SingleItem task="This is the first item" />
-            <SingleItem task="This is the second item" />
+            {list.map((item, index) => {
+              return <SingleItem key={index} task={item.name} />;
+            })}
           </div>
         </div>
       </main>
